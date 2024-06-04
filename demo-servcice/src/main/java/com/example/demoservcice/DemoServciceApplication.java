187f2a06b0acc3bad3a30ebc57164adcdeca6694
jiangyizhong <446765872@qq.com>
2024-06-04 10:25:56 +0800

    提交dome

---
+++ demo-servcice/src/main/java/com/example/demoservcice/DemoServciceApplication.java
@@ -0,0 +1,14 @@
+package com.example.demoservcice;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoServciceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoServciceApplication.class, args);
+    }
+
+}
---
+++ demo-servcice/src/test/java/com/example/demoservcice/DemoServciceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoservcice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoServciceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
